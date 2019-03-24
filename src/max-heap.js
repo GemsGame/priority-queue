d253198fbfce960b188add235ac2d644933b2565
GemsGame <a.masanovets@gmail.com>
2019-03-24 18:17:07 +0300

    initial commit

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,19 +2,29 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+	const newNode = new Node(data,priority);
+	this.insertNode(newNode);
+	this.shiftNodeUp(newNode);
+    
 	}
 
 	pop() {
+		const datacahe = this.detachRoot();
+		const r = this.restoreRootFromLastInsertedNode(datacahe);
+        
+		this.shiftNodeDown(this.root);
+	
 		
 	}
 
 	detachRoot() {
-		
+		this.root = null;
+	    
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
@@ -30,11 +40,14 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root = null,
+		this.parentNodes = []
 	}
 
 	insertNode(node) {
-		
+	
+		this.root = node;
+        
 	}
 
 	shiftNodeUp(node) {
--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,17 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+    if(maxSize){
+		this.maxSize = maxSize;
+	} else {
+		this.maxSize = 30;
+	}
+	this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+	 this.data = data;
+	 this.priority = priority;
 	}
 
 	shift() {
